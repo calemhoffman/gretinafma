81cb1c63131a5ef70ea86a372727b05de474180d
Calem Hoffman <crhoffman@beboplogin3.lcrc.anl.gov>
2018-11-19 09:25:19 -0600

    A number of new bash scripts for sorting / merging a number of files at once. Poor mans parallel computing on_lcrc branch

---
+++ working/gebmerge_parallel.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Our custom function
+merge_func(){
+  echo "Merging in parallel: at file $1"
+ # sleep $1
+  ./gebmerge_lcrc.sh $1
+  echo "Done with file $1"
+}
+# For loop 5 times
+#count=0
+for i in {290..299}
+do
+	merge_func $i & # Put a function in the background
+#	(( count++ ))
+#	if (( count = 10 )); then
+#	    wait
+#	    count=0
+#	fi
+done
+ 
+## Put all cust_func in the background and bash 
+## would wait until those are completed 
+## before displaying all done message
+wait 
+echo "All done"
