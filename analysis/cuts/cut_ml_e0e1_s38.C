855ac0f2b1431d9804efb8d7c4ece7d58a69bba3
Calem Hoffman <calem.hoffman@gmail.com>
2019-11-06 16:09:30 -0600

    lcrc in november

---
+++ analysis/cuts/cut_ml_e0e1_s38.C
@@ -0,0 +1,32 @@
+{
+//========= Macro generated from object: cut_ml_e0e1_s38/cut_ml_e0e1_s38
+//========= by ROOT version6.18/00
+   
+   cutg = new TCutG("cut_ml_e0e1_s38",22);
+   cutg->SetVarX("e[1]");
+   cutg->SetVarY("e[0]");
+   cutg->SetTitle("cut_ml_e0e1_s38");
+   cutg->SetFillStyle(1000);
+   cutg->SetPoint(0,1382.71,2231.12);
+   cutg->SetPoint(1,1188.57,2094.98);
+   cutg->SetPoint(2,912.452,1838.41);
+   cutg->SetPoint(3,659.346,1555.67);
+   cutg->SetPoint(4,473.831,1346.22);
+   cutg->SetPoint(5,391.859,1257.21);
+   cutg->SetPoint(6,319.954,1152.49);
+   cutg->SetPoint(7,309.887,1126.31);
+   cutg->SetPoint(8,304.135,1073.95);
+   cutg->SetPoint(9,332.897,1053);
+   cutg->SetPoint(10,396.173,1042.53);
+   cutg->SetPoint(11,504.031,1100.13);
+   cutg->SetPoint(12,611.889,1210.09);
+   cutg->SetPoint(13,771.518,1398.58);
+   cutg->SetPoint(14,977.167,1602.79);
+   cutg->SetPoint(15,1109.47,1728.45);
+   cutg->SetPoint(16,1290.67,1916.95);
+   cutg->SetPoint(17,1385.59,1979.79);
+   cutg->SetPoint(18,1447.43,2058.33);
+   cutg->SetPoint(19,1441.67,2194.46);
+   cutg->SetPoint(20,1398.53,2246.82);
+   cutg->SetPoint(21,1382.71,2231.12);
+}
